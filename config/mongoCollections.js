f8e3c917095d7a6234c1f4b2d3c7f465c5c0a602
jmanzelm <jmanzelm@stevens.edu>
2018-04-23 14:41:42 -0400

    created file structure

---
+++ config/mongoCollections.js
@@ -0,0 +1,22 @@
+const dbConnection = require("./mongoConnection.js");
+
+
+const getCollectionFn = collection => {
+  let _col = undefined;
+
+  return async () => {
+    if (!_col) {
+      const db = await dbConnection();
+      _col = await db.collection(collection);
+    }
+
+    return _col;
+  };
+};
+
+module.exports = {
+  users: getCollectionFn("users"),
+  investments: getCollectionFn("investments"),
+  cash: getCollectionFn("cash"),
+  debts: getCollectionFn("debts")
+};
---
+++ data/index.js
@@ -0,0 +1,11 @@
+const userData = require("./users");
+const investmentData = require("./investments");
+const cashData = require("./cash");
+const debtData = require("./debts");
+
+module.exports = {
+	users: userData,
+	investments: investmentData,
+	cash: cashData,
+	debts: debtData
+}
