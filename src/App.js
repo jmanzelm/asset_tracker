90a6b87afc802df872a45fde3d51f30e65dec0af
Taylor He <taylor.he7@gmail.com>
2018-05-08 23:02:27 -0400

    barebones code

--- src/App.js
+++ src/App.js
@@ -2,35 +2,12 @@ import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
+import Main from './Main';
 
 class App extends Component {
   render() {
-    let s =       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          DIS APP MY BRO.
-        </p>
-      </div>
-
-    return (
-    <div>
-    {s}
-      <Button bsSize="large"> Hello press me </Button>
-      <table className="table">
-        <thead> 
-          <tr>
-            <th> This is a table header </th>
-          </tr>
-        </thead>
-        <tr> 
-          <td>This is a table cell</td>
-        </tr>
-      </table>
-      </div>
-    );
+    
+    return < Main />
   }
 }
 
