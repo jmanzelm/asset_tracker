0d1d5c615d77538fba50cde2f3d24af64b6ae840
jmanzelm <jmanzelm@stevens.edu>
2018-05-10 17:40:49 -0400

    readded server stuff

--- server/data/debts.js
+++ server/data/debts.js
@@ -1,65 +1,140 @@
 const mongoCollections = require("../config/mongoCollections");
 const debts = mongoCollections.debts;
-const uuid = require("node-uuid");
+const uuidv4 = require("uuid/v4");
+const users = require("./users");
 
-let exportedMethods = {
-  getAllDebts() {
-    return cash().then(debtsCollection => {
-      return debtsCollection.find({}).toArray();
-    });
-  },
-  getDebtsByUserId(id) {
-    return debts().then(debtsCollection => {
-      return debtCollection.find({ user_id: id }).then(debt => {
-        if (!debt) throw "this debt does not exist";
-        return debt;
-      });
-    });
-  },
-  addToDebt(creditor, amount, user_id) {
-    let ts = Math.round((new Date()).getTime() / 1000);
-    return cash().then(debtCollection => {
-        let debt = {
-          creditor: creditor,
-          amount: amount,
-          date: ts,
-          _id: uuid.v4()
-        };
+function getAllDebts() {
+	if (arguments.length !== 0) {
+		throw "No arguments are needed.";
+	}
+	try {
+		return debts().then(debtCollection => {
+			return debtCollection.find({}).toArray();
+		});
+	}
+	catch(error) {
+		throw error;
+	}
+}
 
-        return debtCollection
-          .findOne({user_id: user_id, "creditors.creditor":creditor})
-          .then(existingDebt=>{
-            let newAmount = amount;
-            if (!existingDebt)
-            {
-              newAmount+=amount;
-            }
-            else{
-              existingDebt.creditors.forEach(element => {
-                newAmount+=element.amount;
-              });
-            }
-            return debtCollection.updateOne({"user_id":user_id}, {$push:{ "creditors.creditor": debt}}, {upsert:true})
-                    .then(newId => {
-                      return debtCollection.findOne({ _id: newId }).then(newDebt => {
-                          if (!newDebt) throw "this debt does not exist";
-                          return newDebt;
-                        });
-                    });
-          });
-    });
-  },
-  removeDebt(user_id, creditor) {
-    return cash().then(debtCollection => {
-      return debtCollection.removeOne({ user_id: user_id, "creditors.creditor": creditor }).then(deletionInfo => {
-        if (deletionInfo.deletedCount === 0) {
-          throw `Could not delete debt with id of ${id}`;
-        } else {
-            return "Delete successful";
-        }
-      });
-    });
-  }
-};
+function getDebtById(id) {
+	if (arguments.length !== 1) {
+		throw "Please provide a single ID.";
+	}
+	if (typeof id !== "string") {
+		throw "The ID must be a string.";
+	}
+	try {
+		return debts().then(debtCollection => {
+			return debtCollection.findOne({_id: id}).then(debt => {
+				if (!debt) throw "Debt not found";
+				return debt;
+			});
+		});
+	}
+	catch(error) {
+		throw error;
+	}
+}
 
-module.exports = exportedMethods;
+function addDebt(userId, creditor, startingAmount) {
+	if (arguments.length !== 3) {
+		throw "Please provide a user ID, creditor, and starting amount.";
+	}
+	if (typeof userId !== "string" || typeof creditor !== "string" || typeof startingAmount !== "number"){
+		"The user ID and symbol must be strings and starting amount must be a number."
+	}
+	try{
+		return debts().then(debtCollection => {
+			if (startingAmount <= 0) {
+				return {};
+			}
+			let newDebt = {
+				_id: uuidv4(),
+				creditor: creditor,
+				transactions: [],
+				startingAmount: startingAmount,
+				currentAmount: startingAmount
+			};
+			return debtCollection
+				.insertOne(newDebt)
+				.then(insInfo => {
+					users.extendDebtList(userId, insInfo.insertedId);
+					return insInfo.insertedId;
+				})
+				.then(newId => {
+					return this.getDebtById(newId);
+				});
+		});
+	}
+	catch(error) {
+		throw error;
+	}
+}
+
+function deleteDebt(id, userId) {
+	if (arguments.length !== 2) {
+		throw "Please provide an debt ID and a user ID.";
+	}
+	if (typeof id !== "string") {
+		throw "Both IDs must be strings.";
+	}
+	try {
+		return debts().then(debtCollection => {
+			return debtCollection.removeOne({_id: id}).then(delInfo => {
+				if (delInfo.deletedCount === 0) {
+					throw `Could not remove debt with id of ${id}.`;
+				} else {
+					users.shortenDebtList(userId, id);
+				}
+			});
+		});
+	}
+	catch(error) {
+		throw error;
+	}
+}
+
+// type is true if addition, false if reduction
+function addDebtTransaction(id, userId, quantity, type) {
+	if (arguments.length !== 4) {
+		throw "Please provide an debt ID, user ID, quantity, and type.";
+	}
+	if (typeof id !== "string" || typeof userId !== "string" || typeof quantity !== "number" || typeof type !== "boolean"){
+		throw "The debt ID and user ID must be strings, quantity must be a number, and type must be a boolean.";
+	}
+	try {
+		return debts().then(debtCollection => {
+			debt = this.getDebtById(id);
+			if (!type && debt.currentAmount <= quantity) {
+				this.deleteDebt(id, userId);
+				return {};
+			}
+			newTransaction = {
+				type: type,
+				qty: quantity,
+				date: Math.round((new Date()).getTime() / 1000)
+			};
+			let updatedDebt = {
+				transactions: (this.getDebtById(id)).transactions.push(newTransaction),
+				currentAmount: (type ? debt.currentAmount + quantity : debt.currentAmount - quantity)
+			}
+			return debtCollection
+				.updateOne({_id: id}, {$set: updatedDebt})
+				.then(result => {
+					return this.getDebtById(id);
+				});
+		});
+	}
+	catch(error) {
+		throw error;
+	}
+}
+
+module.exports = {
+	getAllDebts,
+	getDebtById,
+	addDebt,
+	deleteDebt,
+	addDebtTransaction
+}
---
+++ server/data/users.js
@@ -0,0 +1,217 @@
+const mongoCollections = require("../config/mongoCollections");
+const users = mongoCollections.users;
+const uuidv4 = require("uuid/v4");
+const cash = require("./cash");
+const investments = require("./investments");
+const debts = require("./debts");
+
+function getAllUsers() {
+  if (arguments.length !== 0) {
+    throw "No arguments are needed.";
+  }
+  try {
+    return users().then(userCollection => {
+      return userCollection.find({}).toArray();
+    });
+  }
+  catch(error) {
+    throw error;
+  }
+}
+
+function getUserById(id) {
+  if (arguments.length !== 1) {
+    throw "Please provide a single ID.";
+  }
+  if (typeof id !== "string") {
+    throw "The ID must be a string.";
+  }
+  try {
+    return users().then(userCollection => {
+      return userCollection.findOne({_id: id}).then(user => {
+        if (!user) throw "User not found";
+        return user;
+      });
+    });
+  }
+  catch(error) {
+    throw error;
+  }
+}
+
+function addUser(username, hashedPassword, startingCash) {
+  if (arguments.length !== 3) {
+    throw "Please provide a username, hashed password, and starting cash amount.";
+  }
+  if (typeof username !== "string" || typeof hashedPassword !== "string" || typeof startingCash !== "number"){
+    "The username and hashed password must be strings and the starting cash amount must be a number."
+  }
+  try{
+    return users().then(userCollection => {
+      let newUser = {
+        _id: uuidv4(),
+        username: username,
+        hashedPassword: hashedPassword,
+        investments: [],
+        cash: (cash.addCash(startingCash))._id, // add a new cash object
+        debts: []
+      };
+      return userCollection
+        .insertOne(newUser)
+        .then(insInfo => {
+          return insInfo.insertedId;
+        })
+        .then(newId => {
+          return this.getUserById(newId);
+        });
+    });
+  }
+  catch(error) {
+    throw error;
+  }
+}
+
+function deleteUser(id) {
+  if (arguments.length !== 1) {
+    throw "Please provide a single ID";
+  }
+  if (typeof id !== "string") {
+    throw "The ID must be a string.";
+  }
+  try {
+    return users().then(userCollection => {
+      const user = this.getUserById(id);
+      cash.deleteCash(user.cash);
+      ilen = user.investments.length;
+      dlen = user.debts.length;
+      for (i = 0; i < ilen; i++) {
+        investments.deleteInvestment(user.investments[i]);
+      }
+      for (i = 0; i < dlen; i++) {
+        investments.deleteDebt(user.debts[i]);
+      }
+      return userCollection.removeOne({_id: id}).then(delInfo => {
+        if (delInfo.deletedCount === 0) {
+          throw `Could not remove user with id of ${id}.`;
+        } else {
+        }
+      });
+    });
+  }
+  catch(error) {
+    throw error;
+  }
+}
+
+// called when a new investment is created
+function extendInvestmentList(id, investmentId) {
+  if (arguments.length !== 2) {
+    throw "Please provide a user ID and investment ID.";
+  }
+  if (typeof id !== "string" || typeof investmentId !== "string"){
+    throw "The user ID and investment ID must be strings.";
+  }
+  try {
+    return users().then(userCollection => {
+      let updatedUser = {
+        investments: (this.getUserById(id)).investments.push(investmentId)
+      }
+      return userCollection
+        .updateOne({_id: id}, {$set: updatedUser})
+        .then(result => {
+          return this.getUserById(id);
+        });
+    });
+  }
+  catch(error) {
+    throw error;
+  }
+}
+
+// called when a investment is deleted
+function shortenInvestmentList(id, investmentId) {
+  if (arguments.length !== 2) {
+    throw "Please provide a user ID and investment ID.";
+  }
+  if (typeof id !== "string" || typeof investmentId !== "string"){
+    throw "The user ID and investment ID must be strings.";
+  }
+  try {
+    return users().then(userCollection => {
+      const user = this.getUserById(id);
+      let updatedUser = {
+        investments: user.investments.splice(user.investments.findIndex(function(element) {return element === investmentId}), 1)
+      }
+      return userCollection
+        .updateOne({_id: id}, {$set: updatedUser})
+        .then(result => {
+          return this.getUserById(id);
+        });
+    });
+  }
+  catch(error) {
+    throw error;
+  }
+}
+
+// called when a new debt is created
+function extendDebtList(id, debtId) {
+  if (arguments.length !== 2) {
+    throw "Please provide a user ID and debt ID.";
+  }
+  if (typeof id !== "string" || typeof debtId !== "string"){
+    throw "The user ID and debt ID must be strings.";
+  }
+  try {
+    return users().then(userCollection => {
+      let updatedUser = {
+        debts: (this.getUserById(id)).debts.push(debtId)
+      }
+      return userCollection
+        .updateOne({_id: id}, {$set: updatedUser})
+        .then(result => {
+          return this.getUserById(id);
+        });
+    });
+  }
+  catch(error) {
+    throw error;
+  }
+}
+
+// called when a debt is deleted
+function shortenDebtList(id, debtId) {
+  if (arguments.length !== 2) {
+    throw "Please provide a user ID and debt ID.";
+  }
+  if (typeof id !== "string" || typeof debtId !== "string"){
+    throw "The user ID and debt ID must be strings.";
+  }
+  try {
+    return users().then(userCollection => {
+      const user = this.getUserById(id);
+      let updatedUser = {
+        debts: user.debts.splice(user.debt.findIndex(function(element) {return element === debtId}), 1)
+      }
+      return userCollection
+        .updateOne({_id: id}, {$set: updatedUser})
+        .then(result => {
+          return this.getUserById(id);
+        });
+    });
+  }
+  catch(error) {
+    throw error;
+  }
+}
+
+module.exports = {
+  getAllUsers,
+  getUserById,
+  addUser,
+  deleteUser,
+  extendInvestmentList,
+  shortenInvestmentList,
+  extendDebtList,
+  shortenDebtList
+}
