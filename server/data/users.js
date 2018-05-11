82adf3f6193bc7ddb8f7e662c4a3254b5c90008b
jmanzelm <jmanzelm@stevens.edu>
2018-05-11 18:41:05 -0400

    user and invesdtment updates

--- server/data/users.js
+++ server/data/users.js
@@ -12,28 +12,36 @@ const cash = require("./cash");
 const investments = require("./investments");
 const debts = require("./debts");
 
-ModuleA.getAllUsers = function() {
+ModuleA.getAllUsers = async function() {
   if (arguments.length !== 0) {
     throw "No arguments are needed.";
   }
-  try {
+  const userCollection = await users();
+  return await userCollection.find({}).toArray();
+
+  /*try {
     return users().then(userCollection => {
       return userCollection.find({}).toArray();
     });
   }
   catch(error) {
     throw error;
-  }
+  }*/
 }
 
-ModuleA.getUserById = function(id) {
+ModuleA.getUserById = async function(id) {
   if (arguments.length !== 1) {
     throw "Please provide a single ID.";
   }
   if (typeof id !== "string") {
     throw "The ID must be a string.";
   }
-  try {
+  const userCollection = await users();
+  const user1 = await userCollection.findOne({_id: id});
+  if (user1 === null) throw "User not found";
+  return user1;
+
+  /*try {
     return users().then(userCollection => {
       return userCollection.findOne({_id: id}).then(user => {
         if (!user) throw "User not found";
@@ -43,17 +51,32 @@ ModuleA.getUserById = function(id) {
   }
   catch(error) {
     throw error;
-  }
+  }*/
 }
 
-ModuleA.addUser = function(username, hashedPassword, startingCash) {
+ModuleA.addUser = async function(username, hashedPassword, startingCash) {
   if (arguments.length !== 3) {
     throw "Please provide a username, hashed password, and starting cash amount.";
   }
   if (typeof username !== "string" || typeof hashedPassword !== "string" || typeof startingCash !== "number"){
     "The username and hashed password must be strings and the starting cash amount must be a number."
   }
-  try{
+  const userCollection = await users();
+  let newUser = {
+    _id: uuidv4(),
+    username: username,
+    hashedPassword: hashedPassword,
+    investments: [],
+    cash: (await cash.addCash(startingCash))._id, // add a new cash object
+    debts: []
+  };
+  const insertInfo = await userCollection.insertOne(newUser);
+  if (insertInfo.insertedCount === 0) throw "Could not add user";
+
+  const newId = insertInfo.insertedId;
+  return await this.getUserById(newId);
+
+  /*try{
     return users().then(userCollection => {
       let newUser = {
         _id: uuidv4(),
@@ -75,19 +98,39 @@ ModuleA.addUser = function(username, hashedPassword, startingCash) {
   }
   catch(error) {
     throw error;
-  }
+  }*/
 }
 
-ModuleA.deleteUser = function(id) {
+ModuleA.deleteUser = async function(id) {
   if (arguments.length !== 1) {
     throw "Please provide a single ID";
   }
   if (typeof id !== "string") {
     throw "The ID must be a string.";
   }
-  try {
+
+  const userCollection = await users();
+
+  const user1 = await this.getUserById(id);
+  await cash.deleteCash(user.cash);
+  ilen = user.investments.length;
+  dlen = user.debts.length;
+  for (i = 0; i < ilen; i++) {
+    await investments.deleteInvestment(user.investments[i]);
+  }
+  for (i = 0; i < dlen; i++) {
+    await investments.deleteDebt(user.debts[i]);
+  }
+
+  const deletionInfo = await userCollection.removeOne({_id: id});
+
+  if (deletionInfo.deletedCount === 0) {
+      throw `Could not delete user with id of ${id}`;
+  }
+
+  /*try {
     return users().then(userCollection => {
-      const user = this.getUserById(id);
+      const user1 = this.getUserById(id);
       cash.deleteCash(user.cash);
       ilen = user.investments.length;
       dlen = user.debts.length;
@@ -107,18 +150,32 @@ ModuleA.deleteUser = function(id) {
   }
   catch(error) {
     throw error;
-  }
+  }*/
 }
 
 // called when a new investment is created
-ModuleA.extendInvestmentList = function(id, investmentId) {
+ModuleA.extendInvestmentList = async function(id, investmentId) {
   if (arguments.length !== 2) {
     throw "Please provide a user ID and investment ID.";
   }
   if (typeof id !== "string" || typeof investmentId !== "string"){
     throw "The user ID and investment ID must be strings.";
   }
-  try {
+
+  const userCollection = await users();
+  const user1 = await this.getUserById(id);
+  let updatedUser = {
+    investments: user1.investments.push(investmentId)
+  };
+
+  const updateInfo = await userCollection.updateOne({_id: id}, {$set: updatedUser});
+  if (updateInfo.modifiedCount === 0) {
+    throw "could not update dog successfully";
+  }
+
+  return await this.getUserById(id);
+
+  /*try {
     return users().then(userCollection => {
       let updatedUser = {
         investments: (this.getUserById(id)).investments.push(investmentId)
@@ -132,18 +189,32 @@ ModuleA.extendInvestmentList = function(id, investmentId) {
   }
   catch(error) {
     throw error;
-  }
+  }*/
 }
 
 // called when a investment is deleted
-ModuleA.shortenInvestmentList = function(id, investmentId) {
+ModuleA.shortenInvestmentList = async function(id, investmentId) {
   if (arguments.length !== 2) {
     throw "Please provide a user ID and investment ID.";
   }
   if (typeof id !== "string" || typeof investmentId !== "string"){
     throw "The user ID and investment ID must be strings.";
   }
-  try {
+
+  const userCollection = await users();
+  const user1 = await this.getUserById(id);
+  let updatedUser = {
+    investments: user1.investments.splice(user1.investments.findIndex(function(element) {return element === investmentId}), 1)
+  };
+
+  const updateInfo = await userCollection.updateOne({_id: id}, {$set: updatedUser});
+  if (updateInfo.modifiedCount === 0) {
+    throw "could not update dog successfully";
+  }
+
+  return await this.getUserById(id);
+
+  /*try {
     return users().then(userCollection => {
       const user = this.getUserById(id);
       let updatedUser = {
@@ -158,18 +229,32 @@ ModuleA.shortenInvestmentList = function(id, investmentId) {
   }
   catch(error) {
     throw error;
-  }
+  }*/
 }
 
 // called when a new debt is created
-ModuleA.extendDebtList = function(id, debtId) {
+ModuleA.extendDebtList = async function(id, debtId) {
   if (arguments.length !== 2) {
     throw "Please provide a user ID and debt ID.";
   }
   if (typeof id !== "string" || typeof debtId !== "string"){
     throw "The user ID and debt ID must be strings.";
   }
-  try {
+
+  const userCollection = await users();
+  const user1 = await this.getUserById(id);
+  let updatedUser = {
+    debts: user1.debts.push(investmentId)
+  };
+
+  const updateInfo = await userCollection.updateOne({_id: id}, {$set: updatedUser});
+  if (updateInfo.modifiedCount === 0) {
+    throw "could not update dog successfully";
+  }
+
+  return await this.getUserById(id);
+
+  /*try {
     return users().then(userCollection => {
       let updatedUser = {
         debts: (this.getUserById(id)).debts.push(debtId)
@@ -183,18 +268,32 @@ ModuleA.extendDebtList = function(id, debtId) {
   }
   catch(error) {
     throw error;
-  }
+  }*/
 }
 
 // called when a debt is deleted
-ModuleA.shortenDebtList = function(id, debtId) {
+ModuleA.shortenDebtList = async function(id, debtId) {
   if (arguments.length !== 2) {
     throw "Please provide a user ID and debt ID.";
   }
   if (typeof id !== "string" || typeof debtId !== "string"){
     throw "The user ID and debt ID must be strings.";
   }
-  try {
+
+  const userCollection = await users();
+  const user1 = await this.getUserById(id);
+  let updatedUser = {
+    investments: user1.debts.splice(user1.debts.findIndex(function(element) {return element === investmentId}), 1)
+  };
+
+  const updateInfo = await userCollection.updateOne({_id: id}, {$set: updatedUser});
+  if (updateInfo.modifiedCount === 0) {
+    throw "could not update dog successfully";
+  }
+
+  return await this.getUserById(id);
+
+  /*try {
     return users().then(userCollection => {
       const user = this.getUserById(id);
       let updatedUser = {
@@ -209,5 +308,5 @@ ModuleA.shortenDebtList = function(id, debtId) {
   }
   catch(error) {
     throw error;
-  }
+  }*/
 }
