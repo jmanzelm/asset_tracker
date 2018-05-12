4257c8f46d4b099bc25d6151ed29a79f42ec1ab8
Taylor He <taylor.he7@gmail.com>
2018-05-12 19:07:42 -0400

    fds

--- server/app.js
+++ server/app.js
@@ -28,15 +28,15 @@ const saltRounds = 16;
 configRoutes(app);
 
 const main = async function() {
-	const ben = await users.addUser("masterdetective123", "$2a$16$7JKSiEmoP3GNDSalogqgPu0sUbwder7CAN/5wnvCWe6xCKAKwlTD.", 50);
-	const benapple = await investments.addInvestment(ben._id, "AAPL", "stock", 5);
-	const bencoin = await investments.addInvestment(ben._id, "BTC", "crypto", 5);
-	const bendebt = await debts.addDebt(ben._id, "college", 150000)
-	await investments.addInvestmentTransaction(benapple._id, 2, "add");
-	console.log(await users.getAllUsers());
-	console.log(await investments.getAllInvestments());
-	console.log(await cash.getAllCash());
-	console.log(await debts.getAllDebts());
+	// const ben = await users.addUser("masterdetective123", "$2a$16$7JKSiEmoP3GNDSalogqgPu0sUbwder7CAN/5wnvCWe6xCKAKwlTD.", 50);
+	// const benapple = await investments.addInvestment(ben._id, "AAPL", "stock", 5);
+	// const bencoin = await investments.addInvestment(ben._id, "BTC", "crypto", 5);
+	// const bendebt = await debts.addDebt(ben._id, "college", 150000)
+	// await investments.addInvestmentTransaction(benapple._id, 2, "add");
+	// console.log(await users.getAllUsers());
+	// console.log(await investments.getAllInvestments());
+	// console.log(await cash.getAllCash());
+	// console.log(await debts.getAllDebts());
 	/*console.log((await investments.getInvestmentById(benapple._id)).transactions);
 	console.log(await investments.getStockByUserId(ben._id));
 	console.log(await investments.getCryptoByUserId(ben._id));*/
